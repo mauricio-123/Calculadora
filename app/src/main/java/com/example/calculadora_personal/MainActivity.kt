fd947b188b7ce4d1d5a4a5ecbad59db0055b662f
mauricio palacios <mpalacios@coopebombas.com>
2023-11-01 12:00:15 -0500

    version 1.0.1 calculara

--- app/src/main/java/com/example/calculadora_personal/MainActivity.kt
+++ app/src/main/java/com/example/calculadora_personal/MainActivity.kt
@@ -22,89 +22,99 @@ class MainActivity : AppCompatActivity() {
         val numero_nove= findViewById<TextView>(R.id.numero_nove)
         val ponto = findViewById<TextView>(R.id.ponto)
 
-        numero_zero.setOnClickListener { acrescentarUmaExpressao("0",true)}
+          numero_zero.setOnClickListener { acrescentarUmaExpressao("0",true)}
+          numero_um.setOnClickListener{ acrescentarUmaExpressao("1",true) }
+          numero_dois.setOnClickListener{ acrescentarUmaExpressao("2",true) }
+          numero_tres.setOnClickListener{ acrescentarUmaExpressao("3",true) }
+          numero_quatro.setOnClickListener{ acrescentarUmaExpressao("4",true) }
+          numero_cinco.setOnClickListener{ acrescentarUmaExpressao("5",true) }
+          numero_seis.setOnClickListener{ acrescentarUmaExpressao("6",true) }
+          numero_sete.setOnClickListener{ acrescentarUmaExpressao("7",true) }
+          numero_oito.setOnClickListener{ acrescentarUmaExpressao("8",true) }
+          numero_nove.setOnClickListener{ acrescentarUmaExpressao("9",true) }
+          ponto.setOnClickListener { acrescentarUmaExpressao(".",true) }
 
 
+          //operadores
+          val soma = findViewById<TextView>(R.id.soma)
+          soma.setOnClickListener { acrescentarUmaExpressao("+",false) }
+          val subtracao= findViewById<TextView>(R.id.subtracao)
+          subtracao.setOnClickListener { acrescentarUmaExpressao("-",false) }
+          val multiplicacao =
+              findViewById<TextView>(R.id.multiplicacao)
+          multiplicacao.setOnClickListener { acrescentarUmaExpressao("*",false) }
+          val divisao = findViewById<TextView>(R.id.divisao)
+          divisao.setOnClickListener { acrescentarUmaExpressao("/",false) }
 
-
-  numero_um.setOnClickListener{ acrescentarUmaExpressao("1",true) }
-  numero_dois.setOnClickListener{ acrescentarUmaExpressao("2",true) }
-  numero_tres.setOnClickListener{ acrescentarUmaExpressao("3",true) }
-  numero_quatro.setOnClickListener{ acrescentarUmaExpressao("4",true) }
-  numero_cinco.setOnClickListener{ acrescentarUmaExpressao("5",true) }
-  numero_seis.setOnClickListener{ acrescentarUmaExpressao("6",true) }
-  numero_sete.setOnClickListener{ acrescentarUmaExpressao("7",true) }
-  numero_oito.setOnClickListener{ acrescentarUmaExpressao("8",true) }
-  numero_nove.setOnClickListener{ acrescentarUmaExpressao("9",true) }
-  ponto.setOnClickListener { acrescentarUmaExpressao(".",true) }
-
-
-  //operadores
-  val soma = findViewById<TextView>(R.id.soma)
-  soma.setOnClickListener { acrescentarUmaExpressao("+",false) }
-  val subtracao= findViewById<TextView>(R.id.subtracao)
-  subtracao.setOnClickListener { acrescentarUmaExpressao("-",false) }
-  val multiplicacao =
-      findViewById<TextView>(R.id.multiplicacao)
-  multiplicacao.setOnClickListener { acrescentarUmaExpressao("*",false) }
-  val divisao = findViewById<TextView>(R.id.divisao)
-  divisao.setOnClickListener { acrescentarUmaExpressao("/",false) }
-
-  val limpar = findViewById<TextView>(R.id.limpar)
-  val expressao = findViewById<TextView>(R.id.expressao)
-  val text_resultado = findViewById<TextView>(R.id.text_resultado)
-  limpar.setOnClickListener {
-      expressao.text = ""
-      text_resultado.text = ""
-  }
-
-
-  val backspace = findViewById<ImageView>(R.id.backspace)
-  backspace.setOnClickListener {
-      val string = expressao.text.toString()
-
-      if(string.isNotBlank()){
-          expressao.text = string.substring(0,string.length-1)
-      }
-      text_resultado.text = ""
-  }
-
-
- /* val  igual =
-      findViewById<TextView>(R.id.igual)
-  igual.setOnClickListener {
-      try {
-          val expressao = ExpressionBuilder(expressao.text.toString()).build()
-          val resultado = expressao.evaluate()
-          val longResult  = resultado.toLong()
-
-          if(resultado == longResult.toDouble()){
-              text_resultado.text = longResult.toString()
-          }else{
-              text_resultado.text = resultado.toString()
+          val limpar = findViewById<TextView>(R.id.limpar)
+          val expressao = findViewById<TextView>(R.id.expressao)
+          val text_resultado = findViewById<TextView>(R.id.text_resultado)
+          limpar.setOnClickListener {
+              expressao.text = ""
+              text_resultado.text = ""
+              total = 0
           }
-      }catch (e: Exception){}
-  }*/
 
-}
-    @Override
-    fun acrescentarUmaExpressao(string:String, limpar_dados:Boolean) {
-    val  text_resultado=
-        findViewById<TextView>(R.id.text_resultado)
-    val  expressao =
-        findViewById<TextView>(R.id.expressao)
 
-  if(text_resultado.text.isNotEmpty()){
-      expressao.text = ""
-  }
+          val backspace = findViewById<ImageView>(R.id.backspace)
+          backspace.setOnClickListener {
+              val string = expressao.text.toString()
+              total = 0
 
-  if(limpar_dados){
-      text_resultado.text = ""
-      expressao.setText(string).toString()
-  }else{
-      expressao.setText(text_resultado.text).toString()
-      expressao.setText(string).toString()
-      text_resultado.text = ""
-  }
+              if(string.isNotBlank()){
+                  expressao.text = string.substring(0,string.length-1)
+              }
+              text_resultado.text = ""
+
+          }
+
+        val  igual =
+            findViewById<TextView>(R.id.igual)
+        igual.setOnClickListener {
+            try {
+
+                text_resultado.text = total.toString()
+                expressao.text = ""
+                total = 0
+
+            }catch (e: Exception){}
+        }
+
+
+
+    }
+    var total = 0
+    var x = 0
+            fun  ExpressionBuilder(calcular: String, numero: String) {
+                        //suma
+                         if(calcular == "+") {
+                             var suma = numero.toInt()
+                             total = suma.plus(x)
+                             total.toString()
+                         }
+                    }
+            @Override
+            fun acrescentarUmaExpressao(string:String, limpar_dados:Boolean) {
+            val  text_resultado=
+                findViewById<TextView>(R.id.text_resultado)
+            val  expressao =
+                findViewById<TextView>(R.id.expressao)
+             if(text_resultado.text.isNotEmpty()){
+              expressao.text = ""
+            }
+
+            if(limpar_dados){
+                text_resultado.setText(text_resultado.text.toString()+string).toString()
+
+               if (expressao.text != "")
+                    x = text_resultado.text.length
+                print(x)
+            }else{
+              expressao.setText(text_resultado.text).toString()
+              expressao.setText(string).toString()
+              ExpressionBuilder(expressao.text.toString(),text_resultado.text.toString())
+              text_resultado.text = ""
+
+          }
     }
 }
